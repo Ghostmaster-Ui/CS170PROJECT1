2e503a447f8a05b2eabbae2f6fd1466582c3b4c9
Ghostmaster-Ui <asing326@ucr.edu>
2026-02-13 21:01:33 -0800

    update

--- node.py
+++ node.py
@@ -3,4 +3,6 @@ class SearchNode:
         self.current_state = current_state
         self.previous_node = previous_node
         self.move = move
-        
+        self.cost_so_far = cost_so_far
+        self.heuristic_value = heuristic_value
+        self.total_cost = cost_so_far + heuristic_value
